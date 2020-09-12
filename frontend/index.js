20786b71733f49b67b163f9b462281a7827bef1a
Luca Andrei <andreilucalink@gmail.com>
2020-09-12 18:51:33 +0300

    cors-anywhere

--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,9 @@ const SNAKE_COLOR_TWO = "#68b0ab";
 const FOOD_COLOR = "#e66916";
 
 // const socket = io("http://localhost:3000");
-const socket = io("https://sleepy-island-33889.herokuapp.com/");
+const socket = io(
+  "https://cors-anywhere.herokuapp.com/https://sleepy-island-33889.herokuapp.com/"
+);
 
 socket.on("init", handleInit);
 socket.on("gameState", handleGameState);
