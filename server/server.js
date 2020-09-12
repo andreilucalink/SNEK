b34527ef71a58e1263d3c49c41907b68972695b1
Luca Andrei <andreilucalink@gmail.com>
2020-09-12 18:21:30 +0300

    reset

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 const io = require("socket.io")();
 
-// io.origins("*:*");
-
 const { makeid } = require("./utils");
 const { initGame, gameLoop, getUpdatedVelocity } = require("./game");
 const { FRAME_RATE } = require("./constants");
